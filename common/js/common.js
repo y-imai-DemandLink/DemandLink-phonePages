bc2b52cc1d91572168ac65c5b117471b00b492d6
DESKTOP-E8IMCJ4\akonitto <hamakazelove@gmail.com>
2021-06-10 15:26:45 +0900

    no message

--- common/js/common.js
+++ common/js/common.js
@@ -3,7 +3,8 @@ $(function(){
     // よくある質問の表示非表示
     $(".qa-menu-open").click(function(){
         var hideStats = null;
-        var target = $(this).parent().next();
+        var target = $(this).parent().next().children("div:nth-child(1)");
+        console.log(target);
 
         target.toggle(500, "linear");
         target.next().hide(500);
